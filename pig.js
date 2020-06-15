a1e0a91e319cab6f6c7281b5cda8004e7590fa83
Tp-13 <66135230+Tp-13@users.noreply.github.com>
2020-06-15 14:01:51 -0400

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,27 @@
+class Pig {
+  constructor(x, y, width, height) {
+    var options = {
+         restitution: 0.8,
+         friction: 0.3,
+         density: 1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+
+    World.add(world, this.body);
+  }  
+  display(){
+    var pos = this.body.position;
+    var ang = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(ang);
+    rectMode(CENTER);
+    strokeWeight(1);
+    stroke("white");
+    fill("green")
+    rect(0, 0, this.width, this.height);
+    pop();
+  } 
+}
